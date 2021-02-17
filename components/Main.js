c942db6d9c7d83849b742ef97318a701813aeb2f
Emanuel Cardozo <leuname.01@gmail.com>
2021-02-17 16:00:53 -0300

    Addind a folder structure

--- components/Main.js
+++ components/Main.js
@@ -1,36 +1,8 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
-import List from './List'
-import NavigationButtons from './NavigationButtons'
-
-const PER_PAGE = 5
-let slideDirection = 'left'
+import React from 'react'
+import Index from './index/Index'
 
 function Main(){
-
-    const [ pokemons, setPokemons ] = useState([])
-    const [ page, setPage ] = useState(0)
-
-    useEffect(()=>{
-        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${ PER_PAGE }&offset=${ page*10 }`).then((res)=> {
-            return res.json()
-        }).then((json)=> {
-            setPokemons(json.results)
-        }).catch( e => console.log("error", e) )
-    }, [page])
-
-    function changePage(offset){
-        setPokemons([])
-        setPage(page + offset)
-        slideDirection = offset === 1 ? 'right' : 'left'
-    }
-
-    return(
-        <View  style={{ flex: 1 }}>
-            <List pokemons={ pokemons } slideDirection={ slideDirection } />
-            <NavigationButtons prevPage={ changePage.bind(null, -1) } nextPage={ changePage.bind(null, 1) } />    
-        </View>
-    )
+    return(<Index />)
 }
 
 export default Main
---
+++ components/index/Index.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import List from './list/List'
+import NavigationButtons from './NavigationButtons'
+
+const PER_PAGE = 5
+let slideDirection = 'right'
+
+function Index(){
+
+    const [ pokemons, setPokemons ] = useState([])
+    const [ page, setPage ] = useState(0)
+
+    useEffect(()=>{
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${ PER_PAGE }&offset=${ page*10 }`).then((res)=> {
+            return res.json()
+        }).then((json)=> {
+            setPokemons(json.results)
+        }).catch( e => console.log("error", e) )
+    }, [page])
+
+    function changePage(offset){
+        setPokemons([])
+        setPage(page + offset)
+        slideDirection = offset === 1 ? 'right' : 'left'
+    }
+
+    return(
+        <View  style={{ flex: 1 }}>
+            <List pokemons={ pokemons } slideDirection={ slideDirection } />
+            <NavigationButtons prevPage={ changePage.bind(null, -1) } nextPage={ changePage.bind(null, 1) } />    
+        </View>
+    )
+}
+
+export default Index
